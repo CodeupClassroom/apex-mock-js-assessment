33ba4b67a1d63f301358820b9c5983d2779b8711
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-03-20 14:02:03 -0500

    iseven

--- solutions.js
+++ solutions.js
@@ -23,14 +23,40 @@
 
 //function isTrue()
 function isTrue(param) {
-    return param === true ;
+    return param === true;
 }
 
 //function isFalse()
-function isFalse(value){
-    if (value === false){
+function isFalse(value) {
+    if (value === false) {
         return true;
-    } else{
+    } else {
+        return false;
+    }
+}
+
+//Define a function named not that takes in any input and returns the boolean opposite of the provided input
+
+function not(input) {
+    return !input
+}
+
+//Define a function named addOne that takes in a single input. If the input is a number or a numeric string, return the value plus one.
+
+// function addOne(input) {
+//     if (!isNaN(input)) {
+//         return ((input) + 1);
+//     } else if (isNaN(input)){
+//         return parseInt((input)+1);
+//     }
+// }
+//
+//Define a function named isEven that takes in a single input. If the input is an even number or a string containing an even number, return true. Any other input should return false for the output
+
+function isEven(input){
+    if (input % 2 ===0){
+        return true;
+    } else {
         return false;
     }
 }
