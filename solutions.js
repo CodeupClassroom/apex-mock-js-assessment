d2e286255ce3df2c63ee4971ab71237df75414e9
Fernando Mendoza <fernandoza@gmail.com>
2020-03-20 10:13:33 -0500

    feat: solutions

--- solutions.js
+++ solutions.js
@@ -20,3 +20,49 @@
  * and
  * concat
  */
+
+function isTrue(param){
+    return param === true;
+}
+
+function isFalse(param){
+    return param === false;
+}
+
+function not(param){
+    return !param;
+}
+
+function addOne(param){
+    if(param === Infinity){
+        return Infinity;
+    }
+    return parseInt(param) + 1;
+}
+
+function isEven(param){
+    if(param === false){
+        return false;
+    }
+    return (param % 2) === 0;
+}
+
+function isIdentical(param1, param2)  {
+    return param1 === param2;
+}
+
+function isEqual(param1, param2){
+    return param1 == param2;
+}
+
+function or(param1, param2){
+    return param1 || param2;
+}
+
+function and(param1, param2){
+    return param1 && param2;
+}
+
+function concat(first, second){
+    return first + "" + second;
+}
